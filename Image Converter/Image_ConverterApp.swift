d41361b040aa4dd381de05978110d8b94eab017f
Yulin Luo <luoyulin365@gmail.com>
2025-11-05 14:54:58 -0500

    Initial Commit

---
+++ Image Converter/Image_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ConverterApp.swift
+//  Image Converter
+//
+//  Created by Yulin Luo on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Image_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
